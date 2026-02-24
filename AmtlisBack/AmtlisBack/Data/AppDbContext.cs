3d2c3153b19510239edffcba342c4261dce0d71d
Голоцван Нікіта Дмитрович <Golo_bs09>
2026-02-24 19:26:55 +0200

    Initial commit

---
+++ AmtlisBack/AmtlisBack/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AmtlisBack.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmtlisBack.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
